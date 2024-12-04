27db19545c515221481a2538b3ca5f03023b8b9b
bcahalen <bcahalen@gmail.com>
2024-12-04 12:46:40 -0500

    sourced resume in public folder, and created path to it in hyperlink

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
           <!-- <router-link to="/about">About</router-link> -->
           <router-link to="/projects">Projects</router-link>
           <router-link to="/contactme">Contact Me</router-link>
-          <a role="button" href="" download="Barrett Cahalen Resume.pdf">Resume</a>
+          <a role="button" href="../public/Barrett Cahalen Resume.pdf" download="Barrett Cahalen Resume.pdf">Resume</a>
         </nav>
       </div>
     </header>
